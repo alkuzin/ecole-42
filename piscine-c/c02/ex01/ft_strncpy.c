099856b502988a83e963cf6c5e1d1bc033d19c58
alkuzin <alkuzindev@gmail.com>
2023-11-04 22:14:24 +0300

    Update ft_strncpy.c

--- piscine-c/c02/ex01/ft_strncpy.c
+++ piscine-c/c02/ex01/ft_strncpy.c
@@ -6,7 +6,7 @@
 /*   By: alkuzin <[null]@student.42.fr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/04 18:17:19 by alkuzin           #+#    #+#             */
-/*   Updated: 2023/11/04 18:55:25 by alkuzin          ###   ########.fr       */
+/*   Updated: 2023/11/04 22:13:46 by alkuzin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,18 +25,13 @@ int ft_strlen(char *str)
 
 char *ft_strncpy(char *dest, char *src, unsigned int n)
 {
-    int src_len;
-    int pos;
-
-    src_len = ft_strlen(src);
-    if(n > src_len)
-        n = src_len;
-
-    pos = -1;
-    while(src[++pos] && pos < n)
+    int i;
+    
+    i = 0;
+    while(src[i++] && i < n)
     {
-        dest[pos] = src[pos];
+        dest[i] = src[i];
     }
-    dest[pos] = '\0';
+    dest[i] = '\0';
     return dest;
 }
