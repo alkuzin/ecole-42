cb4d5c86682c80064fe52fa7aa1a3b8c48f184b1
alkuzin <alkuzindev@gmail.com>
2023-11-14 18:05:44 +0300

    Added libft.h file

---
+++ libft/libft.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alkuzin <[null]@student.42.fr>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/14 16:07:40 by alkuzin           #+#    #+#             */
+/*   Updated: 2023/11/14 17:39:47 by alkuzin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+#define LIBFT_H
+
+#include "stdio/ft_stdio.h"
+
+typedef unsigned long t_size;
+
+/* The ft_memset() fills the first n bytes of the memory area pointed to by s */
+/* with the constant byte c. */
+extern void *ft_memset(void *s, int c, t_size n);
+
+/*---------------------------------------------------------------*/
+
+#endif /* LIBFT_H */
