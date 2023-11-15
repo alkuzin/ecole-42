7e845dfe45c978e2b6c90f70694a5bb3e40aa8c4
alkuzin <alkuzindev@gmail.com>
2023-11-15 22:08:26 +0300

    Added ctype/ft_ctype.h

---
+++ libft/ctype/ft_ctype.h
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ctype.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alkuzin <[null]@student.42.fr>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/15 19:54:38 by alkuzin           #+#    #+#             */
+/*   Updated: 2023/11/15 20:25:32 by alkuzin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_CTYPE_H
+#define FT_CTYPE_H
+
+/* Checks for an alphabetic character */
+extern int ft_isalpha(int c);
+
+/* Checks for a digit */
+extern int ft_isdigit(int c);
+
+/* Checks for an alphanumeric character */
+extern int ft_isalnum(int c);
+
+/* Checks for an ASCII character */
+extern int ft_isascii(int c);
+
+/* Checks for a printable character (including space) */
+extern int ft_isprint(int c);
+
+/* Converts to uppercase character */
+extern int ft_toupper(int c);
+
+/* Converts to lowercase character */
+extern int ft_tolower(int c);
+
+/* Checks for an uppercase character */
+extern int ft_isupper(int c);
+
+/* Checks for an lowercase character */
+extern int ft_islower(int c);
+
+#endif /* FT_CTYPE_H */
