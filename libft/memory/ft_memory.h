871a997b3d41d76b8907127eb349757683bb393b
alkuzin <alkuzindev@gmail.com>
2023-11-16 18:26:41 +0300

    Added memory/ft_memory.h

---
+++ libft/memory/ft_memory.h
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memory.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alkuzin <[null]@student.42.fr>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/16 15:47:39 by alkuzin           #+#    #+#             */
+/*   Updated: 2023/11/16 18:24:24 by alkuzin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../libft.h"
+
+/* The ft_memset() fills the first n bytes of the 
+ * memory of the area pointed to by s 
+ * with the constant byte c. */
+extern void *ft_memset(void *s, int c, t_size n);
+
+/* The ft_bzero() erases the data in the n bytes 
+ * of the memory of the area pointed to by s, 
+ * by writing '\0' bytes to that area. */
+extern void ft_bzero(void *s, t_size n);
+
+/* The ft_memcpy() copies n bytes from memory area src
+ * to memory area dest. */
+extern void *ft_memcpy(void *dest, const void *src, t_size n);
+
+/* The ft_memccpy() copies no more than n bytes 
+ * from memory area src to memory area dest, 
+ * stopping when the character c is found. */
+extern void *ft_memccpy(void *dest, const void *src, int c, t_size n);
+
+/* The ft_memmove() copies n bytes 
+ * from memory area src to memory area dest. */
+extern void *ft_memmove(void *dest, const void *src, t_size n);
+
+/* The ft_memchr() scans the initial n bytes
+ * of the memory area pointed to by s
+ * for the first instance of c. */
+extern void *ft_memchr(const void *s, int c, t_size n);
