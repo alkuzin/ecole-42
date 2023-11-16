50a8f35968bba6d7bc719e0a2cc5ddec387fbb2e
alkuzin <alkuzindev@gmail.com>
2023-11-16 19:15:54 +0300

    Updated libft.h

--- libft/libft.h
+++ libft/libft.h
@@ -6,16 +6,17 @@
 /*   By: alkuzin <[null]@student.42.fr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/14 16:07:40 by alkuzin           #+#    #+#             */
-/*   Updated: 2023/11/15 21:52:08 by alkuzin          ###   ########.fr       */
+/*   Updated: 2023/11/16 18:46:30 by alkuzin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 #define LIBFT_H
 
+#include <stdlib.h>
+
 #include "stdio/ft_stdio.h"
 #include "ctype/ft_ctype.h"
-
-typedef unsigned long t_size;
+#include "memory/ft_memory.h"
 
 #endif /* LIBFT_H */
