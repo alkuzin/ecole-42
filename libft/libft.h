7d0e957b0f20468e280580bb8add2b50dac61ee2
alkuzin <alkuzindev@gmail.com>
2023-11-17 19:20:56 +0300

    Update libft.h

--- libft/libft.h
+++ libft/libft.h
@@ -14,9 +14,13 @@
 #define LIBFT_H
 
 #include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
 
 #include "stdio/ft_stdio.h"
 #include "ctype/ft_ctype.h"
 #include "memory/ft_memory.h"
+#include "string/ft_string.h"
+#include "stdlib/ft_stdlib.h"
 
 #endif /* LIBFT_H */
