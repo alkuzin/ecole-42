de06661f188ead7f88542f836466a2641ef04a2c
alkuzin <alkuzindev@gmail.com>
2023-11-17 18:30:16 +0300

    Added string/ft_strlcat.c

---
+++ libft/string/ft_strlcat.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alkuzin <[null]@student.42.fr>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/17 16:22:17 by alkuzin           #+#    #+#             */
+/*   Updated: 2023/11/17 17:15:18 by alkuzin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_string.h"
+
+size_t ft_strlcat(char *dest, const char *src, size_t size)
+{
+    size_t i;
+    int dest_len;
+    int src_len;
+
+    
+    dest_len = ft_strlen(dest);
+    src_len = ft_strlen(src);
+
+    if(size <= src_len)
+        return (size + src_len);
+    
+    i = 0;
+    while(src[i] && i < (size - dest_len - 1))
+    {
+        dest[dest_len + i] = src[i];
+        i++;
+    }
+    dest[i + dest_len] = '\0';
+    return (dest_len + src_len);
+}
