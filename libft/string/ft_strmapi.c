d6bf2a912f237e736df76555d851bd94c1356944
alkuzin <alkuzindev@gmail.com>
2023-11-19 11:11:31 +0300

    Added ft_strmapi.c

---
+++ libft/string/ft_strmapi.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alkuzin <[null]@student.42.fr>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/19 10:53:21 by alkuzin           #+#    #+#             */
+/*   Updated: 2023/11/19 11:11:12 by alkuzin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_string.h"
+
+char *ft_strmapi(char const *s, int (*f)(int))
+{
+    char *str;
+    int length;
+    unsigned int i;
+
+    str = ft_strdup(s);
+    if(!str)
+        return NULL;
+
+    i = 0;
+    while(str[i])
+    {
+        str[i] = f(s[i]);
+        i++;
+    }
+
+    return str;
+}
