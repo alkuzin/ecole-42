780436ec9ad7e94e13c7d4aa527f247e200fece8
alkuzin <alkuzindev@gmail.com>
2023-11-19 11:45:03 +0300

    Updated ft_stdio.h

--- libft/stdio/ft_stdio.h
+++ libft/stdio/ft_stdio.h
@@ -6,7 +6,7 @@
 /*   By: alkuzin <[null]@student.42.fr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/14 17:19:26 by alkuzin           #+#    #+#             */
-/*   Updated: 2023/11/14 17:35:57 by alkuzin          ###   ########.fr       */
+/*   Updated: 2023/11/19 11:40:30 by alkuzin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,5 +23,18 @@ extern void ft_putstr(char *str);
 
 /* Print string on the new line */
 extern void ft_puts(char *str);
+
+/* Outputs the character ’c’ to the given file descriptor. */
+extern void ft_putchar_fd(char c, int fd);
+
+/* Outputs the string ’s’ to the given file descriptor. */
+extern void ft_putstr_fd(char *s, int fd);
+
+/* Outputs the string ’s’ to the given file 
+ * descriptor, followed by a newline. */
+extern void ft_putendl_fd(char *s, int fd);
+
+/* Outputs the integer ’n’ to the given file descriptor.*/
+extern void ft_putnbr_fd(int n, int fd);
 
 #endif /* FT_STDIO_H */
